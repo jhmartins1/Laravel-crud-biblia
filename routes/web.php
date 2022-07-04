bb2db95abb9d71b372a8af76f6d2fcbe77528e49
João Heitor <jhmartinsrdo@gmail.com>
2022-07-03 22:29:32 -0300

    rotas web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('web')->group(function () {
+
+    Route::get('/', function () {
+        // return view('welcome');
+        return 'Olá';
+    })->name('variosusuarios');
+
+    Route::get('/{id}', function () {
+        return 'Olá essa tem um id';
+    })->name('apenasumusuario');
 });
